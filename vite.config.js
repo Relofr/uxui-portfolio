1d30a5fa5eaa381a2531b51f7cbd70b5800fd388
Kyle Hatch <ky13h4tch@gmail.com>
2022-07-23 10:40:44 -0600

    adding dist subtree

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: "/uxui-portfolio/",
+  publicPath: process.env.NODE_ENV === "production" ? "/uxui-portfolio/" : "/uxui-portfolio/",
   plugins: [vue()]
 })
