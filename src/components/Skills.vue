e8c41e7248cc7f1619c4010f60f2a22839d52099
Kyle Hatch <ky13h4tch@gmail.com>
2022-07-23 13:20:18 -0600

    Updated skills icons path

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -3,28 +3,28 @@
         <div class="primary-heading">{{ title }}</div>
         <div class="icons-container">
             <Tooltip :text="`Adobe Illustrator`">
-                <img id="icon-image" src="src/assets/icons/ai.png" alt="adobe illustrator">
+                <img id="icon-image" :src="AdobeIllustrator" alt="adobe illustrator">
             </Tooltip>
             <Tooltip :text="`Figma`">
-                <img id="icon-image" src="src/assets/icons/figma.png" alt="figma">
+                <img id="icon-image" :src="Figma" alt="figma">
             </Tooltip>
             <Tooltip :text="`Github`">
-                <img id="icon-image" src="src/assets/icons/github.png" alt="github">
+                <img id="icon-image" :src="Github" alt="github">
             </Tooltip>
             <Tooltip :text="`Invision`">
-                <img id="icon-image" src="src/assets/icons/invision.png" alt="">
+                <img id="icon-image" :src="Invision" alt="">
             </Tooltip>
             <Tooltip :text="`Miro`">
-                <img id="icon-image" src="src/assets/icons/miro.png" alt="">
+                <img id="icon-image" :src="Miro" alt="">
             </Tooltip>
             <Tooltip :text="`Photoshop`">
-                <img id="icon-image" src="src/assets/icons/photoshop.png" alt="">
+                <img id="icon-image" :src="Photoshop" alt="">
             </Tooltip>
             <Tooltip :text="`Vue`">
-                <img id="icon-image" src="src/assets/icons/vue.png" alt="">
+                <img id="icon-image" :src="Vue" alt="">
             </Tooltip>
             <Tooltip :text="`Wireframing`">
-                <img id="icon-image" src="src/assets/icons/wireframe.png" alt="">
+                <img id="icon-image" :src="Wireframing" alt="">
             </Tooltip>
         </div>
     </div>
@@ -32,7 +32,19 @@
 
 <script>
 import Tooltip from './Tooltip.vue'
+import AdobeIllustrator from '../assets/icons/ai.png'
+import Figma from '../assets/icons/figma.png'
+import Github from '../assets/icons/github.png'
+import Invision from '../assets/icons/invision.png'
+import Miro from '../assets/icons/miro.png'
+import Photoshop from '../assets/icons/photoshop.png'
+import Vue from '../assets/icons/vue.png'
+import Wireframing from '../assets/icons/Wireframe.png'
+
 export default {
+    setup: () => {
+        return { AdobeIllustrator, Figma, Github, Invision, Miro, Photoshop, Vue, Wireframing };
+    },
     data() {
         return {}
     },
