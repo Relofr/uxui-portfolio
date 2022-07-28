158e854daf383e4ffdc96618e501170d7fb2c446
Kyle Hatch <ky13h4tch@gmail.com>
2022-07-27 18:55:53 -0600

    Updated css and grid layout of dailyui

--- src/views/DailyUiView.vue
+++ src/views/DailyUiView.vue
@@ -1,44 +1,58 @@
 <template>
     <div>
         <Section sectionTitle="DailyUI" :src="heroDailyUI" />
+        <div class="grid4x4">
 
-        <Section :src2="appleWatch" showImage sectionTitle="Spotify - Apple Watch"
-            contentOne="Made in Figma" contentTwo="" />
+            <div>
+                <Card title="Spotify - Apple Watch" :src="appleWatch" />
+            </div>
 
-        <Section :src2="spotifyMobile" showImage sectionTitle="Spotify Mobile Design"
-            contentOne="Made in Figma" contentTwo="" />
+            <div>
+                <Card title="Spotify Mobile Design" :src="spotifyMobile" />
+            </div>
 
-        <Section :src2="calc" showImage sectionTitle="Calculator"
-            contentOne="Made in Figma" contentTwo="" />
+            <div>
+                <Card title="Calculator" :src="calc" />
+            </div>
 
-        <Section :src2="dgdLandingPage" showImage sectionTitle="DGD Landing Page"
-            contentOne="Made in Figma" contentTwo="" />
+            <div>
+                <Card title="DGD Landing Page" :src="dgdLandingPage" />
+            </div>
 
-        <Section :src2="dotaLeaderboard" showImage sectionTitle="Dota 2 Leaderboards"
-            contentOne="Made in Figma" contentTwo="" />
+            <div>
+                <Card title="Dota 2 Leaderboard" :src="dotaLeaderboard" />
+            </div>
 
-        <Section :src2="grids" showImage sectionTitle="Grid"
-            contentOne="Made in Figma" contentTwo="" />
+            <div>
+                <Card title="Grid System" :src="grids" />
+            </div>
 
-        <Section :src2="inputForm" showImage sectionTitle="Input Form"
-            contentOne="Made in Figma" contentTwo="" />
+            <div>
+                <Card title="Input Form with " :src="inputForm" />
+            </div>
 
-        <Section :src2="pudgePoster" showImage sectionTitle="Dota 2 - Pudge Wanted Poster"
-            contentOne="Made in Figma" contentTwo="" />
+            <div>
+                <Card title="Dota 2 Wanted Poster" :src="pudgePoster" />
+            </div>
 
-        <Section :src2="registrationForm" showImage sectionTitle="Registration Form"
-            contentOne="Made in Figma" contentTwo="" />
+            <div>
+                <Card title="Registration Form" :src="registrationForm" />
+            </div>
 
-        <Section :src2="rwd" showImage sectionTitle="Responsive Web Design"
-            contentOne="Made in Figma" contentTwo="" />
+            <div>
+                <Card title="Responsive Web Design" :src="rwd" />
+            </div>
 
-        <Section :src2="rwd2" showImage sectionTitle="RWD 2"
-            contentOne="Made in Figma" contentTwo="" />
+            <div>
+                <Card title="Responsive Web Design 2" :src="rwd2" />
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import Section from '../components/Section.vue';
+import Card from '../components/Card.vue';
 import SectionBreak from '../components/SectionBreak.vue';
 import heroDailyUI from '../assets/images/dailyui/hero-dailyui.png';
 import appleWatch from '../assets/images/dailyui/apple-watch.png';
@@ -59,9 +73,51 @@ export default {
     setup: () => {
         return { heroDailyUI, appleWatch, calc, dgdLandingPage, dotaLeaderboard, grids, inputForm, pudgePoster, registrationForm, rwd, rwd2, spotifyMobile };
     },
-    components: { Section, SectionBreak }
+    components: { Section, SectionBreak, Card }
 }
 </script>
 
-<style>
+<style lang="less">
+
+.grid4x4 img {
+    height: 100% !important;
+    max-height: 800px !important;
+    object-fit: scale-down !important;
+    box-shadow: none !important;
+}
+.grid4x4 {
+    min-height: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: space-between;
+    pointer-events: none !important;
+}
+
+.grid4x4>div {
+    display: flex;
+    flex-basis: calc(50% - 40px);
+    justify-content: center;
+    flex-direction: column;
+}
+
+.grid4x4>div>div {
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+}
+
+@media (max-width: 800px) {
+    .grid4x4 {
+        display: flex;
+        flex-direction: column;
+
+        .col {
+            width: 100%;
+            margin: 0 0 25px 0;
+            justify-content: center;
+            display: flex;
+        }
+    }
+}
 </style>
